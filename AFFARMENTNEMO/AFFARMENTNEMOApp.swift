f2d6a0a7791f6c2fc047a97cd08afafc8d031c0b
mendoi <mendoi@users.noreply.github.com>
2026-04-29 07:31:33 +0900

    Initial Commit

---
+++ AFFARMENTNEMO/AFFARMENTNEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFFARMENTNEMOApp.swift
+//  AFFARMENTNEMO
+//
+//  Created by TSUBOI AKIRA on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct AFFARMENTNEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFFARMENTNEMOTests/AFFARMENTNEMOTests.swift
@@ -0,0 +1,17 @@
+//
+//  AFFARMENTNEMOTests.swift
+//  AFFARMENTNEMOTests
+//
+//  Created by TSUBOI AKIRA on 2026/04/29.
+//
+
+import Testing
+@testable import AFFARMENTNEMO
+
+struct AFFARMENTNEMOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
